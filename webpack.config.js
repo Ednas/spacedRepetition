25c003cf65269cd751f56c69f7732172ae89b995
codevelopit <codevelopit@gmail.com>
2016-11-03 17:12:07 -0400

    initial setup heroku

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,23 @@
+
 module.exports = {
 
   // This code will be compiled
-  entry: "./app/App.js",
+  entry: __dirname + '/client/src/app.js',
 
   // Then output into this file
+  // this is what would be the
+  // bundle.js file
   output: {
-    filename: "public/bundle.js"
+    path: __dirname + '/client/dist/js',
+    filename: 'app.js',
   },
 
-
+  // This will be what we do
   module: {
     loaders: [
       {
         test: /\.jsx?$/,
-        excluse: /(node_modules|bower_components)/,
+        exclude: /(node_modules|bower_components)/,
         loader: 'babel',
         query: {
           // These are the specific transformations we'll be using.
@@ -21,6 +25,10 @@ module.exports = {
         }
       }
     ]
-  }
+  },
+
+  // start Webpack in a watch mode, so Webpack will rebuild the bundle on changes
+  // this means you just type webpack - instead of webpack w
+  watch: true
 
 }
