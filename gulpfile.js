428f153584f3ebe78d35a1c6fa43965a168eb137
Ednas <edna@edna.tech>
2016-11-03 21:27:06 -0400

    Started gulp file and created the index page

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,10 @@
+var gulp = require('gulp'),
+    browserify = require('gulp-browserify'),
+    webserver = require('gulp-webserver');
+
 var gulp = require('gulp');
 
-gulp.task('default', function() {
-  // place code for your default task here
-});
+//Format for creating Gulp tasks
+// gulp.task('default', function() {
+//   // place code for your default task here
+// });
