01fbc4bfd71625f7760e24cfdc6d36ba60da9702
Ednas <edna@edna.tech>
2016-11-04 09:29:25 -0400

    Added tests mv bundle.js and updated readme file

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,24 @@ var gulp = require('gulp'),
     browserify = require('gulp-browserify'),
     webserver = require('gulp-webserver');
 
-var gulp = require('gulp');
+var src = './app',
+    app = './public';
 
 //Format for creating Gulp tasks
 // gulp.task('default', function() {
 //   // place code for your default task here
 // });
+
+gulp.task('js', function() {
+  return gulp.src( src + 'App.js' )
+    .pipe(browserify({
+      transform: 'reactify',
+      debug: true
+    }))
+    .on('error', function (err) {
+      console.error('Error!', err.message);
+    })
+    .pipe(gulp.dest(app + '/js'));
+});
+
+gulp.task('default', ['js']);
